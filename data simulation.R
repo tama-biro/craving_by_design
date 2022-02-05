98d366a75bf165bca1702c5d67bcff6183857595
tama-biro <samuel.thelaus@gmail.com>
2022-02-05 15:19:02 +0000

    Added varying K

--- data simulation.R
+++ data simulation.R
@@ -8,14 +8,76 @@ simulate_choice = function(sim_data) {
     reward_value = sim_data$reward_value[i]
     uncertainty = sim_data$uncertainty[i]
     
-    v = uncertainty*(win_chance*(reward_value-0.7)^alpha_2-lmbda*loss_chance*0.7^alpha_2)
+    v = uncertainty*(win_chance*(reward_value-0.7)^alpha_1-lmbda*loss_chance*0.7^alpha_2)
     
     p_bet = 1/(1 + exp(-beta*v))
     
-    if(sim_data$craver[i] == 1) {
+    if(sim_data$craver[i]) {
       p_bet = K + (1 - K)*p_bet
     }
     sim_data$choice[i] = rbinom(1, 1, p_bet)
+  }
+  return(sim_data)
+}
+
+simulate_choice_vk = function(sim_data) {
+  
+  back_k = 0
+  for(i in 1:nrow(sim_data)) {
+    win_chance = sim_data$win_chance[i]
+    loss_chance = 1 - win_chance
+    reward_value = sim_data$reward_value[i]
+    uncertainty = sim_data$uncertainty[i]
+    
+    v = uncertainty*(win_chance*(reward_value-0.7)^alpha_1-lmbda*loss_chance*0.7^alpha_2)
+    
+    p_bet = 1/(1 + exp(-beta*v))
+    
+    
+    if(sim_data$craver[i]) {
+      K = 0
+      if(back_k > 0) {
+        
+        # implementation of varying K from Payzan-LeNestour & Doran (2022), 1.2.2
+        u = sum(if_else(sim_data$outcome[seq(i-back_k,i-1)] > 0, 1, 0)*theta^(i-seq(i-back_k,i-1)))
+        
+        K = 1/(1+exp(-kappa_1*u))-kappa_2
+        
+      }
+      
+      if(K < 0) {
+        K = 0
+      }
+      
+      p_bet = K + (1 - K)*p_bet
+    }
+
+    # Make choice
+    sim_data$choice[i] = rbinom(1, 1, p_bet)
+    
+    # Play game
+    if(sim_data$choice[i]) {
+      if(rbinom(1, 1, uncertainty)) {
+        sim_data$outcome[i] = rbinom(1, 1, win_chance)*reward_value - 0.7
+      } else {
+        sim_data$outcome[i] = 0
+      }
+    } else {
+      sim_data$outcome[i] = 0
+    }
+    
+    # Set steps back for K variable
+    if(sim_data$outcome[i] < 0) {
+      back_k = 0
+    } else {
+      back_k = back_k + 1
+    }
+    
+    if(i != nrow(sim_data)) {
+      if(sim_data$ID[i] != sim_data$ID[i+1]) {
+        back_k = 0
+      }
+    }
   }
   return(sim_data)
 }
@@ -97,7 +159,11 @@ proc.time() - tm
 
 # Set up labels and plot betting rates
 data_plot <- data_compare %>%
-  filter(lmbda > 1.94 & lmbda < 1.96 & alpha == 0.8 & craver == 1)
+  filter(lmbda > 1.94 & 
+           lmbda < 1.96 & 
+           alpha == 0.8 &
+           K == 0.1 &
+           craver == 0)
 
 win_labs <- c('Yellow', 'Blue')
 names(win_labs) <- c(0.2, 0.8)
@@ -109,39 +175,97 @@ ggplot(data_plot, aes(x = beta, y = betting_rate, color = factor(K))) +
   geom_errorbar(aes(ymin = betting_rate - se, ymax = betting_rate + se),
                 width = 0.6) +
   facet_wrap(vars(win_chance), labeller = labeller(win_chance = win_labs)) +
-  labs(x = 'Beta', y = 'Betting Rate', title = 'Craver: lambda = 1, alpha 1 and 2 = 1') +
-  scale_y_continuous(breaks = seq(0, 1, 0.1)) +
-  theme_minimal() +
-  theme(panel.grid.major = element_line(color='black'))
+  labs(x = 'Beta', y = 'Betting Rate', title = 'Optimal: lambda = 1.95, alpha 1 and 2 = 0.8') +
+  scale_y_continuous(breaks = seq(0, 1, 0.05)) +
+  theme_minimal()
 
-ggsave('risk_neutral_craver.png', width = 15, height = 13)
+ggsave('lambda1-95_alpha0-8_optimal.png', width = 10, height = 7)
 
 
 
 # Plot lambda across K
 data_plot_lam <- data_compare %>%
-  group_by(win_chance, craver, beta, lmbda) %>%
+  filter(craver == 1 &
+           alpha == 0.8) %>%
+  group_by(win_chance, beta, lmbda) %>%
   summarize(betting_rate = mean(betting_rate),
             se = mean(se))
 
-crave_labs <- c('Optimal', 'Craver')
-names(crave_labs) <- 0:1
 win_labs <- c('Yellow', 'Blue')
 names(win_labs) <- c(0.2, 0.8)
 
 ggplot(data_plot_lam, aes(x = beta, y = betting_rate, color = factor(lmbda))) +
   geom_hline(yintercept = 0, color = 'gray') +
   geom_vline(xintercept = 0, color = 'gray') +
-  geom_line() +
-  geom_errorbar(aes(ymin = betting_rate - se, ymax = betting_rate + se)) +
-  facet_grid(vars(win_chance), vars(craver),
-             labeller = labeller(craver = crave_labs,
-                                 win_chance = win_labs)) +
-  scale_y_continuous(limits = c(0, 1)) +
-  labs(x = 'Beta', y = 'Betting Rate', title = 'Varying lambda') +
+  geom_line(size = 0.6) +
+  geom_errorbar(aes(ymin = betting_rate - se, ymax = betting_rate + se),
+                width = 0.6) +
+  facet_wrap(vars(win_chance), labeller = labeller(win_chance = win_labs)) +
+  labs(x = 'Beta', y = 'Betting Rate', title = 'Craver: Varying lambda') +
+  scale_y_continuous(breaks = seq(0, 1, 0.05)) +
   theme_minimal()
 
 ggsave('varying_lambda.png', width = 8, height = 7)
+
+
+#### Varying K version
+
+
+# Setting up parameters
+alpha_1 = alpha_2 = 0.8
+lmbda = 1.95
+beta = 30
+K = 0
+theta = 0.9
+kappa_1 = seq(0.1, 1, 0.1)
+kappa_2 = seq(0.5, 0.7, 0.025)
+
+parameters = expand_grid(kappa_1, kappa_2)
+
+tm <- proc.time()
+
+data_compare2 = data.frame()
+for(i in 1:nrow(parameters)) {
+  kappa_1 = parameters$kappa_1[i]
+  kappa_2 = parameters$kappa_2[i]
+  
+  sim_data = simulate_choice_vk(sim_data)
+  
+  data_cc = sim_data %>%
+    group_by(win_chance, craver) %>%
+    summarize(betting_rate = mean(choice),
+              se = se(choice)) %>%
+    ungroup() %>%
+    mutate(kappa_1 = kappa_1,
+           kappa_2 = kappa_2)
+  
+  data_compare2 = rbind(data_compare2, data_cc)
+  
+  print(i)
+}
+
+proc.time() - tm
+
+
+data_plot <- data_compare2 %>%
+  filter(craver == 1 &
+           !grepl('25', as.character(kappa_2)) &
+           !grepl('75', as.character(kappa_2)))
+
+win_labs <- c('Yellow', 'Blue')
+names(win_labs) <- c(0.2, 0.8)
+
+ggplot(data_plot, aes(x = kappa_1, y = betting_rate, color = factor(kappa_2))) +
+  geom_hline(yintercept = 0, color = 'gray') +
+  geom_vline(xintercept = 0, color = 'gray') +
+  geom_line(size = 0.6) +
+  geom_errorbar(aes(ymin = betting_rate - se, ymax = betting_rate + se),
+                width = 0.6) +
+  facet_wrap(vars(win_chance), labeller = labeller(win_chance = win_labs)) +
+  labs(x = 'Kappa 1', y = 'Betting Rate', title = 'Optimal: lambda = 1.95, alpha 1 and 2 = 0.8') +
+  scale_y_continuous(breaks = seq(0, 1, 0.05)) +
+  theme_minimal()
+
 
 
 
