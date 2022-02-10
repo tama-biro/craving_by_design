ac1ce25995d79f8488b7d045b62ff56f3ea861f8
tama-biro <samuel.thelaus@gmail.com>
2022-02-10 21:12:17 +0000

    New varying K and plots
    Added negative loss case for varying K case and updated plotting to have expressions in axis labels. Moved K calculation to after bet.

--- data simulation.R
+++ data simulation.R
@@ -33,21 +33,10 @@ simulate_choice_vk = function(sim_data) {
     
     p_bet = 1/(1 + exp(-beta*v))
     
-    
     if(sim_data$craver[i]) {
-      K = 0
-      if(back_k > 0) {
-        
-        # implementation of varying K from Payzan-LeNestour & Doran (2022), 1.2.2
-        u = sum(if_else(sim_data$outcome[(i-back_k):(i-1)] > 0, 1, 0)*theta^(i-(i-back_k):(i-1)))
-        
-        K = max(1/(1+exp(-kappa_1*u))-kappa_2, 0)
-        
-      }
-      
       p_bet = K + (1 - K)*p_bet
     }
-
+    
     # Make choice
     sim_data$choice[i] = rbinom(1, 1, p_bet)
     
@@ -60,6 +49,23 @@ simulate_choice_vk = function(sim_data) {
       }
     } else {
       sim_data$outcome[i] = 0
+    }
+    
+    
+    if(sim_data$craver[i]) {
+      K = 0
+      if(back_k > 0) {
+        
+        # implementation of varying K from Payzan-LeNestour & Doran (2022), 1.2.2
+        u = sum(if_else(sim_data$outcome[(i-back_k):i] > 0, 1, 0)*theta^(i-(i-back_k):i))
+        
+        K = max(1/(1+exp(-kappa_1*u))-kappa_2, 0)
+        
+        if(K < 0 | K > 1) {
+          print(K)
+        }
+        
+      }
     }
     
     # Set steps back for K variable
@@ -123,6 +129,67 @@ simulate_choice_vk2 = function(sim_data) {
       }
     } else {
       sim_data$outcome[i] = 0
+    }
+    
+    # Set steps back for K variable
+    if(sim_data$outcome[i] < 0) {
+      back_k = 0
+    } else {
+      back_k = back_k + 1
+    }
+    
+    if(i != nrow(sim_data)) {
+      if(sim_data$ID[i] != sim_data$ID[i+1]) {
+        back_k = 0
+      }
+    }
+  }
+  return(sim_data)
+}
+
+
+simulate_choice_vk_neg_loss = function(sim_data) {
+  
+  back_k = 0
+  for(i in 1:nrow(sim_data)) {
+    win_chance = sim_data$win_chance[i]
+    loss_chance = 1 - win_chance
+    reward_value = sim_data$reward_value[i]
+    uncertainty = sim_data$uncertainty[i]
+    
+    v = uncertainty*(win_chance*(reward_value-0.7)^alpha_1-lmbda*loss_chance*0.7^alpha_2)
+    
+    p_bet = 1/(1 + exp(-beta*v))
+    
+    if(sim_data$craver[i]) {
+      p_bet = K + (1 - K)*p_bet
+    }
+    
+    # Make choice
+    sim_data$choice[i] = rbinom(1, 1, p_bet)
+    
+    # Play game
+    if(sim_data$choice[i]) {
+      if(rbinom(1, 1, uncertainty)) {
+        sim_data$outcome[i] = rbinom(1, 1, win_chance)*reward_value - 0.7
+      } else {
+        sim_data$outcome[i] = 0
+      }
+    } else {
+      sim_data$outcome[i] = 0
+    }
+    
+    
+    if(sim_data$craver[i]) {
+      K = 0
+      if(back_k > 0) {
+        
+        # implementation of varying K from Payzan-LeNestour & Doran (2022), 1.2.2
+        u = sum(if_else(sim_data$outcome[(i-back_k):i] > 0, 1, -1)*theta^(i-(i-back_k):i))
+        
+        K = max(1/(1+exp(-kappa_1*u))-kappa_2, 0)
+        
+      }
     }
     
     # Set steps back for K variable
@@ -222,27 +289,27 @@ data_plot <- data_compare %>%
            lmbda < 1.96 & 
            alpha == 0.8 &
 #           K == 0.1 &
-           craver == 1)
+           craver == 1 &
+           win_chance == 0.8)
 
 data_plot <- data_compare %>%
   filter(lmbda == 1 &
            alpha == 1 &
            K == 0.1 &
-           craver == 0)
+           craver == 0 )
 
-win_labs <- c('Yellow', 'Blue')
-names(win_labs) <- c(0.2, 0.8)
-
-ggplot(data_plot, aes(x = beta, y = betting_rate, col = factor(K))) +
+ggplot(data_plot, aes(x = beta, y = betting_rate, color = factor(K))) +
   geom_line(size = 0.6) +
   geom_errorbar(aes(ymin = betting_rate - se, ymax = betting_rate + se),
                 width = 0.6) +
-  facet_wrap(vars(win_chance), labeller = labeller(win_chance = win_labs),
-             scales='free_y') +
-  labs(x = 'Beta', y = 'Betting Rate', title = 'Cravers: lambda = 1.95, alpha 1 and 2 = 0.8') +
-  theme_minimal()
+  scale_y_continuous(expand = c(0, 0), breaks = seq(0, 1, by = 0.05)) +
+  labs(x = expression(beta), 
+       y = 'Betting Rate', 
+       title = expression("Cravers (blue):" ~ lambda ~ "= 1.95," ~ alpha[1] ~ "=" ~ alpha[2] ~ "= 0.8")) +
+  theme_minimal() +
+  guides(color=guide_legend(title='K factor'))
 
-ggsave('lambda1-95_alpha0-8_cravers.png', width = 10, height = 7)
+ggsave('lambda1-95_alpha0-8_cravers_blue.png', width = 10, height = 7)
 
 
 
@@ -264,7 +331,7 @@ ggplot(data_plot_lam, aes(x = beta, y = betting_rate, color = factor(lmbda))) +
   geom_errorbar(aes(ymin = betting_rate - se, ymax = betting_rate + se),
                 width = 0.6) +
   facet_wrap(vars(win_chance), labeller = labeller(win_chance = win_labs)) +
-  labs(x = 'Beta', y = 'Betting Rate', title = 'Craver: Varying lambda') +
+  labs(x = expression(beta), y = 'Betting Rate', title = expression("Craver: Varying" ~ lambda)) +
   scale_y_continuous(breaks = seq(0, 1, 0.05)) +
   theme_minimal()
 
@@ -287,7 +354,7 @@ parameters = expand_grid(kappa_1, kappa_2)
 
 tm <- proc.time()
 
-data_compare3 = data.frame()
+data_compare2 = data.frame()
 for(i in 1:nrow(parameters)) {
   kappa_1 = parameters$kappa_1[i]
   kappa_2 = parameters$kappa_2[i]
@@ -302,7 +369,7 @@ for(i in 1:nrow(parameters)) {
     mutate(kappa_1 = kappa_1,
            kappa_2 = kappa_2)
   
-  data_compare3 = rbind(data_compare3, data_cc)
+  data_compare2 = rbind(data_compare2, data_cc)
   
   print(i)
 }
