ba0923d0c929a3d61e6515b4f5ad7c9e3c4504a3
tama_biro <samuel.thelaus@t-dab.com>
2022-12-04 15:12:57 +0000

    add first yellow

---
+++ main_analysis/adhoc_uncertainty.R
@@ -0,0 +1,57 @@
+library(tidyverse)
+
+# SE function
+se <- function (x) {
+  se = sd(x, na.rm = TRUE)/sqrt(length(x))
+  return(se)
+}
+
+# 1. Betting rate in yellow is higher in control
+d1_plot <- data %>%
+  filter(block_type == "C") %>%
+  mutate(treatment = factor(treatment, levels = c("control", "test"),
+                            labels = c("Control", "Test"))) %>%
+  group_by(id, treatment) %>%
+  summarize(betting_rate = mean(choice)) %>%
+  ungroup %>%
+  group_by(treatment) %>%
+  summarize(mean = mean(betting_rate),
+            se = se(betting_rate)) %>%
+  ungroup
+
+ggplot(d1_plot, aes(x = treatment, y = mean)) +
+  geom_bar(stat = "identity", fill = '#f7ef25', width = 0.6) +
+  geom_errorbar(aes(ymin = mean - se, ymax = mean + se),
+                width = 0.1) +
+  labs(x = 'Treatment', y = 'Mean betting rate') +
+  theme_minimal()
+
+
+# 2. Betting rate in yellow control by uncertainty
+d2_plot <- data %>%
+  filter(block_type == "C") %>%
+  mutate(treatment = factor(treatment, levels = c("control", "test"),
+                            labels = c("Control", "Test")),
+         uncertainty = factor(aaron_mood, levels = c("Low", "High"),
+                              labels = c("Low", "High"))) %>%
+  group_by(id, treatment, uncertainty) %>%
+  summarize(betting_rate = mean(choice)) %>%
+  ungroup %>%
+  group_by(treatment, uncertainty) %>%
+  summarize(mean = mean(betting_rate),
+            se = se(betting_rate)) %>%
+  ungroup
+
+ggplot(d2_plot, aes(x = treatment, y = mean, fill = uncertainty)) +
+  geom_bar(stat = "identity", width = 0.6,
+           position = position_dodge(preserve = "single")) +
+  geom_errorbar(aes(ymin = mean - se, ymax = mean + se),
+                width = 0.1,
+                position = position_dodge(width = 0.6, preserve = "single")) +
+  labs(x = 'Treatment', y = 'Mean betting rate') +
+  theme_minimal()
+
+
+
+
+
--- main_analysis/modeling_script.R
+++ main_analysis/modeling_script.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 library(moments)
 library(EnvStats)
+library(blme)
+library(BayesianFirstAid)
 
 
 filepath <- '../data/data_reduced.csv'
@@ -38,21 +40,38 @@ shapiro.test(data_1$betting_rate_bc)
 # Run test
 t.test(data_1$betting_rate_bc ~ reward_value, data = data_1)
 
+t.test(data_1$betting_rate ~ reward_value, data = data_1)
 
 
 #### Test 2 ####
 
 # Logistic mixed effects regression, trial level
 # Predicting betting
+data_2 <- data %>%
+  mutate(reward_value = factor(reward_value, levels = c("Low", "High"),
+                               labels = c("Low", "High")),
+         uncertainty = factor(aaron_mood, levels = c("Low", "High"),
+                              labels = c("Low", "High")),
+         treatment = factor(treatment, levels = c("control", "test"),
+                            labels = c("Control", "Test")),
+         color = factor(block_type, levels = c("C", "S"),
+                        labels = c("Yellow", "Blue")))
 
+log_mod_2 <- bglmer(choice ~ reward_value + uncertainty + treatment * color +
+                      age + gender + major + (1 | id),
+                    data = data_2, family = binomial(link = "logit"))
 
+summary(log_mod_2)
 
 #### Test 3 ####
 
 # Logistic mixed effects regression, trial level
 # Predicting betting, adding reward exposure
+log_mod_3 <- bglmer(choice ~ reward_value + uncertainty + treatment * color +
+                      age + gender + major + exposure_time (1 | id),
+                    data = data_2, family = binomial(link = "logit"))
 
-
+summary(log_mod_3)
 
 #### Test 4 ####
 
@@ -65,25 +84,86 @@ t.test(data_1$betting_rate_bc ~ reward_value, data = data_1)
 
 # Logistic mixed effect model, trial level
 # Predicting betting in yellow (craving)
+data_5 <- data %>%
+  filter(block_type == "C" & craver_2 == 1) %>%
+  mutate(reward_value = factor(reward_value, levels = c("Low", "High"),
+                               labels = c("Low", "High")),
+         uncertainty = factor(aaron_mood, levels = c("Low", "High"),
+                              labels = c("Low", "High")),
+         treatment = factor(treatment, levels = c("control", "test"),
+                            labels = c("Control", "Test")))
 
+log_mod_5 <- bglmer(choice ~ reward_value + uncertainty + treatment +
+                      previous_choice + (1 | id), data = data_5,
+                    family = binomial(link = "logit"))
 
+summary(log_mod_5)
 
 #### Test 6 ####
 
 # Bayesian t-test, participant level
 # H1: Betting rate in yellow, test is > 0
 # H0: Betting rate in yellow, test is 0
+data_6 <- data %>%
+  filter(block_type == "C" & treatment == "test") %>%
+  group_by(id) %>%
+  summarize(betting_rate = mean(choice)) %>%
+  ungroup
 
+# Check skew and normality assumption
+skewness(sqrt(data_6$betting_rate))
+shapiro.test(data_6$betting_rate)
+
+
+# Box-Cox transform
+out <- boxcox(data_6$betting_rate + 1, lambda = seq(-25, 25, by = 0.25))
+out$lambda[which.max(out$objective)]
+
+data_6$betting_rate_bc <- boxcoxTransform(data_6$betting_rate + 1, lambda = -4.75)
+
+skewness(sqrt(data_6$betting_rate_bc))
+shapiro.test(data_6$betting_rate_bc)
+
+# Run test
+bayes.t.test(x = data_6$betting_rate_bc, mu = 0)
+print(paste('Bayes factor:', .682/.318))
+
+bayes.t.test(x = data_6$betting_rate, mu = 0)
+print(paste('Bayes factor:', .621/.379))
 
 
 #### Test 7 ####
 
 # Paired, one-tailed t-test, participant level
 # Betting rate in low/high uncertainty
+data_7 <- data %>%
+  mutate(uncertainty = factor(aaron_mood, levels = c("Low", "High"),
+                               labels = c("Low", "High"))) %>%
+  filter(block_type == "C") %>%
+  group_by(id, uncertainty) %>%
+  summarize(betting_rate = mean(choice)) %>%
+  ungroup
 
 
+# Check skew and normality assumption
+skewness(sqrt(data_7$betting_rate))
+shapiro.test(data_7$betting_rate)
 
 
+# Box-Cox transform
+out <- boxcox(data_7$betting_rate + 1, lambda = seq(-25, 25, by = 0.25))
+out$lambda[which.max(out$objective)]
+
+data_7$betting_rate_bc <- boxcoxTransform(data_7$betting_rate + 1, lambda = -9)
+
+skewness(sqrt(data_7$betting_rate_bc))
+shapiro.test(data_7$betting_rate_bc)
+
+
+# Run test
+t.test(data_7$betting_rate_bc ~ uncertainty, data = data_7)
+
+t.test(data_7$betting_rate ~ uncertainty, data = data_7)
 
 
 
